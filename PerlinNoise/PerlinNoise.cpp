cc9cd2f13ca6000caa3bf8ff04d32cecb5a0f9a3
阿斯德芙 <a1220710481@outlook.com>
2020-01-30 12:03:03 +0800

    LuminousDots

--- PerlinNoise/PerlinNoise.cpp
+++ PerlinNoise/PerlinNoise.cpp
@@ -2,13 +2,11 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <random>
+#include <iostream>
 //#include <iostream>
 //#include <algorithm>
 //#include <ctype.h>
-#include "tbb/tbb.h"
-#include "tbb/task_scheduler_init.h"
 //
-using namespace tbb;
 using namespace cv;
 using namespace std;
 
@@ -49,137 +47,88 @@ void PeilinNoise2D(Mat& img, const vector<vector<Vec2f>>& GridGradient, const si
 					float n11 = g11.dot(Mat{ tx - 1,ty - 1 });
 					float n0 = Interpolate(n00, n10, tx, f);
 					float n1 = Interpolate(n01, n11, tx, f);
-					img.at<float>(y* img.rows / ngird + iy, x* img.cols / ngird + ix) = abs((float)Interpolate(n0, n1, ty, f));
+					img.at<float>(y* img.rows / ngird + iy, x* img.cols / ngird + ix) = (float)Interpolate(n0, n1, ty, f);
 				}
 			}
 		}
 	}
 }
 
-//int main(int argc, char** argv)
-//{
-//	size_t width = 800, height = 600;
-//	size_t ngird = 10;
-//	std::default_random_engine generator(time(nullptr));
-//	std::uniform_real_distribution<double> distribution(-1.0, 1.0);
-//
-//	//task_scheduler_init init;
-//	namedWindow(szWinName);
-//
-//	vector<vector<Vec2f>> GridGradient(ngird + 1);
-//
-//	for (auto &row : GridGradient)
-//	{
-//		row.resize(ngird + 1);
-//		for (auto &i : row)
-//		{
-//			float nx = distribution(generator);
-//			float ny = distribution(generator);
-//			i = Vec2f(nx, ny) / sqrt(nx *nx + ny * ny);
-//		}
-//	}
-//
-//	float theta = 0;
-//	for(;;)
-//	{
-//		theta += 0.05;
-//		//	size_t Ndata = 200;
-//		Mat rot = rotation(theta);
-//		Mat img = Mat::zeros(Size(width, height), CV_32FC1);
-//		//Mat sum = Mat::zeros(Size(width, height), CV_32FC1);
-//		//for (int i = 0; i != 5; ++i)
-//		//{
-//		//	//Mat img = Mat::zeros(Size(width, height), CV_32FC1);
-//		//	
-//		//	ngird = pow(2, i) * 3;
-//		//	vector<vector<Vec2f>> GridGradient(ngird + 1);
-//
-//		//	for (auto &row : GridGradient)
-//		//	{
-//		//		row.resize(ngird + 1);
-//		//		for (auto &i : row)
-//		//		{
-//		//			float nx = distribution(generator);
-//		//			float ny = distribution(generator);
-//		//			i = Vec2f(nx, ny) / sqrt(nx *nx + ny * ny);
-//		//		}
-//		//	}
-//
-//		//	PeilinNoise2D(img, GridGradient, ngird);
-//		//	//convertScaleAbs(img, img);
-//		//	sum += img / ngird;
-//		//}
-//		//PeilinNoise2D(img, GridGradient, ngird);
-//
-//		
-//		parallel_for(blocked_range<size_t>(0, GridGradient[0].size() - 1, 1),
-//			[&](const blocked_range<size_t> &r)
-//		{
-//			for(size_t x = r.begin(); x != r.end(); ++x)
-//			{
-//				for(int ix = 0; ix < ngird; ix++)
-//				{
-//					const float tx = float(ix) / float(ngird);
-//					parallel_for(blocked_range<size_t>(0, GridGradient.size() - 1, 1),
-//						[&](const blocked_range<size_t> &r)
-//					{
-//						for(size_t y = r.begin(); y != r.end(); ++y)
-//						{
-//							//for(int y = 0; y != GridGradient.size() - 1; ++y)
-//						//	{
-//							Mat g00 = rot*GridGradient[y][x];
-//							Mat g01 = rot*GridGradient[y][x + 1];
-//							Mat g10 = rot*GridGradient[y + 1][x];
-//							Mat g11 = rot*GridGradient[y + 1][x + 1];
-//							for(int iy = 0; iy < ngird; iy++)
-//							{
-//								const float ty = float(iy) / float(ngird);
-//								float n00 = g00.dot(Mat{ tx,ty });
-//								float n10 = g01.dot(Mat{ tx - 1,ty });
-//								float n01 = g10.dot(Mat{ tx,ty - 1 });
-//								float n11 = g11.dot(Mat{ tx - 1,ty - 1 });
-//								float n0 = Interpolate(n00, n10, tx, f);
-//								float n1 = Interpolate(n01, n11, tx, f);
-//								img.at<float>(y*ngird + iy, x*ngird + ix) = (float)Interpolate(n0, n1, ty, f);// 255 * min(max(Interpolate(n0, n1, ty, f), 0.0), 1.0);
-//							}
-//						}
-//					});
-//				}
-//			}
-//		});
-//		
-//		/*for(int x = 0; x != GridGradient[0].size() - 1; ++x)
-//		{
-//		}*/
-//		//	img.at<uchar>(x, y) = 255;
-//		//img = sum;
-//		normalize(img, img, 0.0, 1.0, NORM_MINMAX);
-//
-//		img = (img * 255);
-//		img.convertTo(img, CV_8UC1);
-//		applyColorMap(img, img, COLORMAP_RAINBOW);
-//
-//		imshow(szWinName, img);
-//		char c = (char)waitKey(1);
-//	}
-//
-//	return 0;
-//}
+inline void rounded_rectangle(Mat& src,const Point& topLeft,const Point& bottomRight, const Scalar lineColor, Scalar fillColor,
+						const int thickness, const int cornerRadius, const int lineType = LINE_8)
+{
+	/* corners:
+	 * p1 - p2
+	 * |     |
+	 * p4 - p3
+	 */
+	Point p1 = topLeft;
+	Point p2 = Point(bottomRight.x, topLeft.y);
+	Point p3 = bottomRight;
+	Point p4 = Point(topLeft.x, bottomRight.y);
 
+	// draw straight lines
+	line(src, Point(p1.x + cornerRadius, p1.y), Point(p2.x - cornerRadius, p2.y), lineColor, thickness, lineType);
+	line(src, Point(p2.x, p2.y + cornerRadius), Point(p3.x, p3.y - cornerRadius), lineColor, thickness, lineType);
+	line(src, Point(p4.x + cornerRadius, p4.y), Point(p3.x - cornerRadius, p3.y), lineColor, thickness, lineType);
+	line(src, Point(p1.x, p1.y + cornerRadius), Point(p4.x, p4.y - cornerRadius), lineColor, thickness, lineType);
+
+	// draw arcs
+	ellipse(src, p1 + Point(cornerRadius, cornerRadius), Size(cornerRadius, cornerRadius), 180.0, 0, 90, lineColor, thickness, lineType);
+	ellipse(src, p2 + Point(-cornerRadius, cornerRadius), Size(cornerRadius, cornerRadius), 270.0, 0, 90, lineColor, thickness, lineType);
+	ellipse(src, p3 + Point(-cornerRadius, -cornerRadius), Size(cornerRadius, cornerRadius), 0.0, 0, 90, lineColor, thickness, lineType);
+	ellipse(src, p4 + Point(cornerRadius, -cornerRadius), Size(cornerRadius, cornerRadius), 90.0, 0, 90, lineColor, thickness, lineType);
+
+	Point fillFrom(topLeft.x + 10, topLeft.y + 10);
+	floodFill(src, fillFrom, fillColor);
+
+	//line(src, Point(p1.x + cornerRadius, p1.y), Point(p2.x - cornerRadius, p2.y), lineColor, thickness, LINE_AA);
+	//line(src, Point(p2.x, p2.y + cornerRadius), Point(p3.x, p3.y - cornerRadius), lineColor, thickness, LINE_AA);
+	//line(src, Point(p4.x + cornerRadius, p4.y), Point(p3.x - cornerRadius, p3.y), lineColor, thickness, LINE_AA);
+	//line(src, Point(p1.x, p1.y + cornerRadius), Point(p4.x, p4.y - cornerRadius), lineColor, thickness, LINE_AA);
+}
+
+template <typename T = std::size_t>
+constexpr T generate_ith_number(const std::size_t index) {
+	static_assert(std::is_integral<T>::value, "T must to be an integral type");
+
+	//if (index == 0) return 2.0;
+	return 190 - index * 3;
+}
+
+template <std::size_t... Is>
+constexpr auto make_sequence_impl(std::index_sequence<Is...>)
+{
+	return std::index_sequence<generate_ith_number(Is)...>{};
+}
+
+template <std::size_t N>
+constexpr auto make_sequence()
+{
+	return make_sequence_impl(std::make_index_sequence<N>{});
+}
+template <std::size_t... Is>
+constexpr auto make_array_from_sequence_impl(std::index_sequence<Is...>)
+{
+	return std::array<double, sizeof...(Is)>{Is...};
+}
+
+template <typename Seq>
+constexpr auto make_array_from_sequence(Seq)
+{
+	return make_array_from_sequence_impl(Seq{});
+}
 int main(int argc, char** argv)
 {
-	size_t width = 800, height = 600;
-	size_t ngird = 50;
-	std::default_random_engine generator;
+	const size_t width = 800, height = 600;
+	const size_t ngird = 8;
+	std::default_random_engine generator(time(nullptr));
 	std::uniform_real_distribution<double> distribution(-1.0, 1.0);
+	vector<vector<Vec2f>> GridGradient(ngird + 1);
 
-	task_scheduler_init init;
-	namedWindow(szWinName);
-
-	vector<vector<Vec2f>> GridGradient(height / ngird + 1);
 	for (auto &row : GridGradient)
 	{
-		row.resize(width / ngird + 1);
+		row.resize(ngird + 1);
 		for (auto &i : row)
 		{
 			float nx = distribution(generator);
@@ -187,76 +136,62 @@ int main(int argc, char** argv)
 			i = Vec2f(nx, ny) / sqrt(nx *nx + ny * ny);
 		}
 	}
-	parallel_for(blocked_range<size_t>(0, GridGradient[0].size() - 1, 1),
-		[](const blocked_range<size_t> &r)
-	{
-		for (size_t x = r.begin(); x != r.end(); ++x)
-		{
 
+	Mat substrate = Mat::zeros(Size(width, height), CV_32FC1);
+	PeilinNoise2D(substrate, GridGradient, ngird);
+
+	normalize(substrate, substrate, 0.0, 1.0, NORM_MINMAX);
+	substrate = (substrate*255);
+	substrate.convertTo(substrate, CV_8UC1);
+	applyColorMap(substrate, substrate, COLORMAP_OCEAN);//COLORMAP_RAINBOW
+
+	//size_t nx = 32, ny = 24;
+	const size_t lbox = 20;
+	const size_t lgap = 4;
+	Mat img = Mat::zeros(Size(width, height), CV_8UC3);
+
+	for(size_t x = 0;x<=width-lbox-lgap;x+=lbox+lgap)
+	{
+		for(size_t y = 0;y<=height-lbox-lgap;y+=lbox+lgap)
+		{
+			Point lt(x, y);
+			Point rb = lt + Point(lbox, lbox);
+			Mat roi = substrate(Rect(lt, rb));
+			Scalar color(mean(roi));
+			rounded_rectangle(img, lt, rb, color*1.5, color, 2, 5);
 		}
-	});
-	float theta = 0;
-	for (;;)
-	{
-		theta += 0.05;
-		//	size_t Ndata = 200;
-		Mat rot = rotation(theta);
-		Mat img = Mat::zeros(Size(width, height), CV_32FC1);
-
-
-		parallel_for(blocked_range<size_t>(0, GridGradient[0].size() - 1, 1),
-			[&](const blocked_range<size_t> &r)
-		{
-			for (size_t x = r.begin(); x != r.end(); ++x)
-			{
-				for (int ix = 0; ix < ngird; ix++)
-				{
-					const float tx = float(ix) / float(ngird);
-					parallel_for(blocked_range<size_t>(0, GridGradient.size() - 1, 1),
-						[&](const blocked_range<size_t> &r)
-					{
-						for (size_t y = r.begin(); y != r.end(); ++y)
-						{
-
-							//for(int y = 0; y != GridGradient.size() - 1; ++y)
-						//	{
-							Mat g00 = rot * GridGradient[y][x];
-							Mat g01 = rot * GridGradient[y][x + 1];
-							Mat g10 = rot * GridGradient[y + 1][x];
-							Mat g11 = rot * GridGradient[y + 1][x + 1];
-							for (int iy = 0; iy < ngird; iy++)
-							{
-								const float ty = float(iy) / float(ngird);
-								float n00 = g00.dot(Mat{ tx,ty });
-								float n10 = g01.dot(Mat{ tx - 1,ty });
-								float n01 = g10.dot(Mat{ tx,ty - 1 });
-								float n11 = g11.dot(Mat{ tx - 1,ty - 1 });
-								float n0 = Interpolate(n00, n10, tx, f);
-								float n1 = Interpolate(n01, n11, tx, f);
-								img.at<float>(y*ngird + iy, x*ngird + ix) = (float)Interpolate(n0, n1, ty, f);// 255 * min(max(Interpolate(n0, n1, ty, f), 0.0), 1.0);
-							}
-						}
-					});
-				}
-			}
-		});
-
-		/*for(int x = 0; x != GridGradient[0].size() - 1; ++x)
-		{
-		}*/
-		//	img.at<uchar>(x, y) = 255;
-		normalize(img, img, 0.0, 1.0, NORM_MINMAX);
-		//cout << img;
-		img = (img * 255);
-		img.convertTo(img, CV_8UC1);
-		applyColorMap(img, img, COLORMAP_RAINBOW);
-
-
-		imshow(szWinName, img);
-		char c = (char)waitKey(1);
-		if (c == 27)
-			break;
 	}
+
+	constexpr auto magnification = make_array_from_sequence(make_sequence<30>());
+
+	size_t pos = 0;
+	size_t row = 8;
+	Mat temp = img.clone();
+	for(;;)
+	{
+		//substrate = img.clone();
+		img = temp.clone();
+
+		for (size_t i = 0; i != magnification.size() && pos>=i; ++i)
+		{
+			if(pos-i>= width / (lbox + lgap))continue;
+			Point lt((pos-i)*(lbox+lgap), row*(lbox+lgap));
+			Point rb = lt + Point(lbox, lbox);
+			Mat roi = substrate(Rect(lt, rb));
+			//roi *= magnification[i];
+			Scalar color(mean(roi)*magnification[i]/100.f);
+			rounded_rectangle(img, lt, rb, color*1.5, color, 2, 5);
+		}
+
+		pos++;
+		if (pos == width/(lbox+lgap)+magnification.size())pos = 0;
+
+		//GaussianBlur(img, img, Size(3,3), 0.5);
+		imshow(szWinName, img);
+		waitKey(80);
+	}
+
 
 	return 0;
 }
+
